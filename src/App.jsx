95493f1c46f93a3a2e866130d634a771fb18e0d1
leonardo <leonardo_trifiletti@yahoo.it>
2023-12-07 15:16:11 +0100

    es2 done

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,30 @@ function App() {
   const listaOrdinata = ["valore1", "valore2", "valore3"];
   const listaNonOrdinata = ["valore4", "valore5", "valore6"];
 
+  const allLists = [
+    {
+      title: "Invitati",
+      type: "ul",
+      list: ["Tizio", "Caio"],
+    },
+    {
+      title: "Invitati1",
+      type: "ol",
+      list: ["Sempronio", "Gigio"],
+    },
+  ];
+
   return (
     <>
+      {allLists.map((elem, i) => {
+        return (
+          <div>
+            <h1>{elem.title}</h1>
+
+            <CustomList type={elem.type} list={elem.list} />
+          </div>
+        );
+      })}
       <CustomList type="ul" list={listaNonOrdinata} />
       <CustomList type="ol" list={listaOrdinata} />
     </>
