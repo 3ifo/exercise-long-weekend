32289833bf8d3ad7c819db429d25dd666e270d14
leonardo <leonardo_trifiletti@yahoo.it>
2023-12-10 21:44:37 +0100

    es 2,3,4 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,57 @@
-import { useState } from "react";
-import "./App.css";
+import React, { useState } from "react";
 import CustomList from "./components/CustomList";
 
-function App() {
-  const listaOrdinata = ["valore1", "valore2", "valore3"];
-  const listaNonOrdinata = ["valore4", "valore5", "valore6"];
-
-  const allLists = [
+const App = () => {
+  const [allLists, setAllLists] = useState([
     {
       title: "Invitati",
       type: "ul",
       list: ["Tizio", "Caio"],
+      buttons: ["Elimina", "Modifica"],
+      newItem: "",
     },
-    {
-      title: "Invitati1",
-      type: "ol",
-      list: ["Sempronio", "Gigio"],
-    },
-  ];
+  ]);
 
   return (
-    <>
+    <div>
       {allLists.map((elem, i) => {
+        const { newItem } = elem;
         return (
-          <div>
+          <div key={i}>
             <h1>{elem.title}</h1>
-
-            <CustomList type={elem.type} list={elem.list} />
+            <div>
+              <input
+                value={elem.newItem}
+                onChange={(e) => {
+                  setAllLists([...allLists]);
+                  elem.newItem = e.target.value;
+                }}
+                type="text"
+                name=""
+                id=""
+              />
+              <button
+                onClick={() => {
+                  setAllLists([...allLists]);
+                  elem.list = [...elem.list, newItem];
+                  elem.newItem = "";
+                }}
+              >
+                Aggiungi
+              </button>
+              <CustomList
+                type="ul"
+                list={elem.list}
+                buttons={elem.buttons}
+                allLists={allLists}
+                setAllLists={setAllLists}
+              />
+            </div>
           </div>
         );
       })}
-      <CustomList type="ul" list={listaNonOrdinata} />
-      <CustomList type="ol" list={listaOrdinata} />
-    </>
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/CustomList.jsx
+++ src/components/CustomList.jsx
@@ -1,7 +1,34 @@
-const CustomList = ({ type, list }) => {
-  const uList = type === "ul";
-  const listItems = list.map((elem, i) => <li key={i}>{elem}</li>);
-  return uList ? <ul>{listItems}</ul> : <ol>{listItems}</ol>;
+const CustomList = ({ type, list, buttons, allLists, setAllLists }) => {
+  if (type === "ul") {
+    return (
+      <ul>
+        {list.map((elemento, indice) => {
+          return (
+            <li key={indice}>
+              {elemento}
+              <button onClick={() => setAllLists([newItem, ...allLists])}>
+                {buttons[0]}
+              </button>
+              <button>{buttons[1]}</button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  } else {
+    return (
+      <ol>
+        {list.map((elemento, indice) => {
+          return (
+            <li key={indice}>
+              {elemento}
+              <button>{buttons}</button>
+            </li>
+          );
+        })}
+      </ol>
+    );
+  }
 };
 
 export default CustomList;
